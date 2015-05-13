1039b3ef30c72443a8d61b485ede749a0fdd8d5e
vivianj <jiangyy12@gmail.com>
2015-05-12 21:50:43 -0400

    add angular js

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  get 'signup' => 'user#new'
+  resources :users
 end
